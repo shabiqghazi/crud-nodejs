05a0b7cab72d2d74d4000784034b0dc515bd0da3
shabiqghazi <sgarkaan02@gmail.com>
2022-08-09 22:01:57 +0700

    tes-session-cookie

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { homePage, addUser, updateUser } = require("../services/render");
+const { homePage, addUser, updateUser, setSession,getSession,destroySession,setCookie,getCookie,destroyCookie } = require("../services/render");
 const controller = require("../controller/controller");
 
 router.get("/", homePage);
 router.get("/add_user", addUser);
 router.get("/update_user", updateUser);
+router.get("/session", setSession);
+router.get("/view-session", getSession);
+router.get("/destroy", destroySession);
+router.get("/cookie", setCookie);
+router.get("/view-cookie", getCookie);
+router.get("/destroy-cookie", destroyCookie);
 
 // API ROUTES
 router.post("/api/users", controller.addUser);
